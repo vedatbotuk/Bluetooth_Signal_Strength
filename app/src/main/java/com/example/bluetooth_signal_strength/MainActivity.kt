b3304671208cd4581e9bec1b8749e18fa122e6ef
Vedat <vedat.botuk@gmail.com>
2024-10-08 10:21:17 +0200

    first commit

---
+++ app/src/main/java/com/example/bluetooth_signal_strength/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.bluetooth_signal_strength
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bluetooth_signal_strength.ui.theme.Bluetooth_Signal_StrengthTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Bluetooth_Signal_StrengthTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Bluetooth_Signal_StrengthTheme {
+        Greeting("Android")
+    }
+}
